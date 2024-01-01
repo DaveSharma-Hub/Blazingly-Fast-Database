6bd90a7c49053efb88148b2f068255cc3a91eff9
DaveSharma-Hub <davesharma12345@outlook.com>
2023-12-31 22:36:54 -0700

    arguments

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Custom relational SQL database created in Golang.
 ```./database -m=<MAX_CACHED_ITEMS>```
 
 Arguments
--m = Possible number of maximum cached items
+
+--mc = Possible number of maximum cached items for the LRU in-memory cache
 
 WIP
--- database/commandArgs/commandArgs.go
+++ database/commandArgs/commandArgs.go
@@ -7,16 +7,24 @@ import (
 )
 
 func ParseInput(arguments []string)globalTypes.CommandLineArguments{
-	var cacheSize int64 = 1000
-	
+	var commandLineArgs globalTypes.CommandLineArguments
 	for i := 0; i<len(arguments) ; i++ {
 		arg := strings.Split(arguments[i],"=")
-		size, err := strconv.ParseInt(arg[1], 10, 64)
-		if err == nil {
-			cacheSize = size
-		}else{
-			panic(err)
+		var argType string = arg[0]
+		var argValue string = arg[1]
+		switch argType{
+		case "--mc":
+			size, err := strconv.ParseInt(argValue, 10, 64)
+			if err == nil {
+				commandLineArgs.CacheMaxSize = size
+			}else{
+				panic(err)
+				commandLineArgs.CacheMaxSize = 10000
+			}
+			
+		default:
+			// do nothing
 		}
 	}
-	return globalTypes.CommandLineArguments{CacheMaxSize: cacheSize}
+	return commandLineArgs
 }
